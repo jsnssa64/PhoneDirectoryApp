b4e1ceb1a0e91bea345da49daadf69f4580c3ebb
JSDance <jsnsousa92@hotmail.com>
2021-12-09 14:47:32 +0000

    rename module to component

--- app/phoneDirectory/phonedirectory.component.js
+++ app/phoneDirectory/phonedirectory.component.js
@@ -1,4 +1,6 @@
-app.Module("phoneDirectoryApp").Component('phoneDirectory', {
+app
+    .Module("phoneDirectoryApp")
+        .Component('phoneDirectory', {
     template: "phoneDirectory/phoneDirectory.template.js",
     controller: function phoneDirectoryController(){
     this.phonelist = [{number: "123-312-123"},
